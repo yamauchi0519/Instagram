5ddbe699045679b3315425644184603d11107dbc
yamauchi0519 <hirado.daisuke@gmail.com>
2023-03-27 16:03:27 +0900

    formを部分テンプレートに変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, presence: true
+  
+    def display_created_at
+      I18n.l(self.created_at, format: :default)
+    end
+  end
