a8f940d58f55e1b47456edda7bd10777419fa430
yamauchi0519 <hirado.daisuke@gmail.com>
2023-03-29 01:50:26 +0900

    add original validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,31 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :content, presence: true
-  
-    def display_created_at
-      I18n.l(self.created_at, format: :default)
-    end
+  validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 100 }
+  validates :title, format: { with: /\A(?!\@)/ }
+
+  validates :content, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :content, uniqueness: true
+
+  validate :validate_title_and_content_length
+
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
   end
+
+  private
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    errors.add(:content, '100文字いじょうで！') unless char_count > 100
+  end
+end
